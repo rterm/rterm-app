4a9c7e68142a737307ff085448fe66ab1bfcd18f
Ly Nguyen <lyntc.elec@gmail.com>
2020-11-16 17:18:58 +0700

    - Change app name

--- app/menu.ts
+++ app/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About rTerm',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide rTerm',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
